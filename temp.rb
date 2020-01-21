23f909766e42729656d4e4e9b9694bc7fe769715
Khalid T <khalid.tissire@gmail.com>
2020-01-21 20:57:19 +0000

    Automatically backed up by Learn

--- temp.rb
+++ temp.rb
@@ -1,3 +1,25 @@
+$hash, $subhash, $subArr = {}, {}, []
+def nyc_pigeon_organizer(data)
+  pigeons = [data[:gender][:male],data[:gender][:female]].flatten
+  pigeons.each_with_index { |pigeon,index| 
+    $subhash = {} ;
+    process_data(pigeon,data); 
+    return $hash if index === pigeons.length-1
+  }
+end
+def process_data(pigeon,data)
+  data.each { |k,v|
+    $subArr = [];
+    $hash[pigeon] = fetch_pigeon_data(pigeon,k,v)
+    }
+end
+def fetch_pigeon_data(pigeon,k,v)
+  v.each { |subk,subv| 
+    $subhash[k] = $subArr.push(subk.to_s) if subv.include?(pigeon);
+    return $subhash if $subhash.size === 3
+    }
+end
+######################################################
 def nyc_pigeon_organizer(data)
   pigeons = [data[:gender][:male],data[:gender][:female]].flatten
   hash = {}
